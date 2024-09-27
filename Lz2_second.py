998537888d9d32bd685fb42a7da5c1fd8b070bec
sniper504 <abracadabra52@tutamail.com>
2024-09-27 11:26:59 +0300

    task number 1

---
+++ Lz2_second.py
@@ -0,0 +1,2 @@
+print("hello world")
+print("heloo world 2")
---
+++ lesson3-1-part.py
@@ -0,0 +1,8 @@
+input_data=open("input.txt","r")
+data=input_data.read()
+data+data.split()
+a=int(data[0])
+b=int(data[1])
+c=str(a+b)
+output_data=open("output.txt","w")
+output_data=write(c)
